96a350c027b2a3673229fc52c2659b461538f243
sso <sall@w-ss.io>
2022-04-16 10:23:28 +0100

    modified:   .editorconfig 	modified:   .github/README.md 	modified:   build.sh

--- .github/README.md
+++ .github/README.md
@@ -4,22 +4,22 @@ The module is a binary Zsh module (think about `zmodload` Zsh command, it's that
 automatically **compiles sourced scripts**. Many plugin managers do not offer compilation of plugins, the module is
 a solution to this. Even if a plugin manager does compile plugin's main script (like ZI does).
 
-# Installation
+## Installation
 
-## Without [ZI](https://github.com/z-shell/zi)
+### Without [ZI](https://github.com/z-shell/zi)
 
 Install just the **standalone** binary which can be used with anyother plugin manager.
 
 > **[?]**
 > This script can be used with most plugin managers and [ZI](https://github.com/z-shell/zi) is not required.
 
-```bash
-bash <(curl -fsSL https://raw.githubusercontent.com/z-shell/zpmod/HEAD/build.sh)
+```sh
+sh -c <(curl -fsSL https://raw.githubusercontent.com/z-shell/zpmod/HEAD/build.sh)
 ```
 
 This script will display what to add to `~/.zshrc` (2 lines) and show usage instructions.
 
-## With [ZI](https://github.com/z-shell/zi)
+### With [ZI](https://github.com/z-shell/zi)
 
 > **[?]**
 > ZI users can build the module by issuing following command instead of running above `build.sh` script.
@@ -30,7 +30,7 @@ zi module build
 
 This command will compile the module and display instructions on what to add to `~/.zshrc`.
 
-# Measuring Time of sources
+## Measuring Time of sources
 
 Besides the compilation-feature, the module also measures **duration** of each script sourcing.
 Issue `zpmod source-study` after loading the module at top of `~/.zshrc` to see a list of all sourced files with the time the
@@ -38,10 +38,10 @@ sourcing took in milliseconds on the left.
 This feature allows to profile the shell startup. Also, no script can pass-through that check and you will obtain a complete list of all loaded scripts,
 like if Zshell itself was investigating this. The list can be surprising.
 
-# Debugging
+## Debugging
 
 To enable debug messages from the module set:
 
-```zsh
+```shell
 typeset -g ZI_MOD_DEBUG=1
 ```
--- build.sh
+++ build.sh
@@ -1,69 +1,99 @@
 #!/usr/bin/env sh
 
-col_pname="[33m"
-col_error="[31m"
-col_info="[32m"
-col_info2="[32m"
-col_rst="[0m"
-
-printf '%s\n' "${col_info}Re-run this script to update (from Github) and rebuild the module.$col_rst"
-
 #
 # Clone or pull
 #
 
-ZI_HOME="${ZI_HOME:-${ZDOTDIR:-${HOME}}/.zi}"
-MOD_HOME="${MOD_HOME:-zmodules}/zpmod"
+setup_zpmod_repository() {
+  if ! test -d "${ZI_HOME}/${MOD_HOME}"; then
+    mkdir -p "${ZI_HOME}/${MOD_HOME}"
+    chmod g-rwX "${ZI_HOME}/${MOD_HOME}"
+  fi
 
-if ! test -d "$ZI_HOME/$MOD_HOME"; then
-	mkdir -p "$ZI_HOME/$MOD_HOME"
-	chmod g-rwX "$ZI_HOME/$MOD_HOME"
-fi
-
-printf '%s\n' ">>> Downloading ZPMOD module to $ZI_HOME/$MOD_HOME"
-if test -d "$ZI_HOME/$MOD_HOME/.git"; then
-	cd "$ZI_HOME/$MOD_HOME" || return
-	git pull origin main
-else
-	cd "$ZI_HOME" || return
-	git clone https://github.com/z-shell/zpmod.git "$MOD_HOME"
-fi
-printf '%s\n' ">>> Done"
+  printf '%s\n' ">>> Downloading ZPMOD module to ${ZI_HOME}/${MOD_HOME}"
+  if test -d "${ZI_HOME}/${MOD_HOME}/.git"; then
+    cd "${ZI_HOME}/${MOD_HOME}" || return
+    git pull -q origin main
+  else
+    cd "$ZI_HOME" || return
+    git clone -q https://github.com/z-shell/zpmod.git "$MOD_HOME"
+  fi
+  printf '%s\n' ">>> Done"
+}
 
 #
 # Build the module
 #
 
-cd "$ZI_HOME/$MOD_HOME" || return
-printf '%s\n' "$col_pname== Building module ZPMOD, running: a make clean, then ./configure and then make ==$col_rst"
-printf '%s\n' "$col_pname== The module sources are located at: $ZI_HOME/$MOD_HOME ==$col_rst"
-if test -f Makefile; then
-	if [ "$1" = "--clean" ]; then
-		printf '%s\n' "$col_info2-- make distclean --$col_rst"
-		make distclean
-		true
-	else
-		printf '%s\n' "$col_info2-- make clean (pass --clean to invoke \`make distclean') --$col_rst"
-		make clean
-	fi
-fi
-printf '%s\n' "$col_info2-- Configuring --$col_rst"
-run_make="$(CPPFLAGS=-I/usr/local/include CFLAGS="-g -Wall -O3" LDFLAGS=-L/usr/local/lib ./configure --disable-gdbm --without-tcsetpgrp)"
-if [ "$run_make" ]; then
-	printf '%s\n' "$col_info2-- make --$col_rst"
-	run_make2="$(make)"
-	if [ "$run_make2" ]; then
-			command cat <<-EOF
-			[38;5;219m▓▒░[0m [38;5;220mModule [38;5;177mhas been built correctly.
-			[38;5;219m▓▒░[0m [38;5;220mTo [38;5;160mload the module, add following [38;5;220m2 lines to [38;5;172m.zshrc, at top:
-			[0m [38;5;51m module_path+=( "$ZI_HOME/$MOD_HOME/Src" )
-			[0m [38;5;51m zmodload zi/zpmod
-			[38;5;219m▓▒░[0m [38;5;220mSee 'zpmod -h' for more information.
-			[38;5;219m▓▒░[0m [38;5;220mRun 'zpmod source-study' to see profile data,
-			[38;5;219m▓▒░[0m [38;5;220mGuaranteed, automatic compilation of any sourced script.
-		EOF
-	else
-		printf '%s\n' "${col_error}Module didn't build.$col_rst. You can copy the error messages and submit"
-		printf '%s\n' "error-report at: https://github.com/z-shell/zpmod/issues"
-	fi
-fi
+build_zpmod_module() {
+  if command -v zsh >/dev/null; then
+    printf '%s\n' "$col_info2-- Checkig version --$col_rst"
+    ZSH_CURRENT=$(zsh --version </dev/null | head -n1 | cut -d" " -f2,6- | tr -d '-')
+    ZSH_REQUIRED="5.8.1"
+    if expr "$ZSH_CURRENT" \< "$ZSH_REQUIRED" >/dev/null; then
+      printf '%s\n' "$col_error-- Zsh version 5.8.1 and above required --$col_rst"
+      exit 1
+    else
+      printf '%s\n' "$col_info2-- Zsh version $ZSH_CURRENT --$col_rst"
+      cd "${ZI_HOME}/${MOD_HOME}" || return
+      printf '%s\n' "$col_pname== Building module ZPMOD, running: a make clean, then ./configure and then make ==$col_rst"
+      printf '%s\n' "$col_pname== The module sources are located at: ${ZI_HOME}/${MOD_HOME} ==$col_rst"
+      if test -f Makefile; then
+        if [ "$1" = "--clean" ]; then
+          printf '%s\n' "$col_info2-- make distclean --$col_rst"
+          make distclean >/dev/null
+          true
+        else
+          printf '%s\n' "$col_info2-- make clean (pass --clean to invoke \`make distclean') --$col_rst"
+          make clean >/dev/null
+        fi
+      fi
+      printf '%s\n' "$col_info2-- Configuring --$col_rst"
+      if CPPFLAGS=-I/usr/local/include CFLAGS="-g -Wall -O3" LDFLAGS=-L/usr/local/lib ./configure --disable-gdbm --without-tcsetpgrp >/dev/null; then
+        printf '%s\n' "$col_info2-- Running make --$col_rst"
+        if make >/dev/null; then
+          command cat <<-EOF
+[38;5;219m▓▒░[0m [38;5;220mModule [38;5;177mhas been built correctly.
+[38;5;219m▓▒░[0m [38;5;220mTo [38;5;160mload the module, add following [38;5;220m2 lines to [38;5;172m.zshrc, at top:
+
+[0m [38;5;51m module_path+=( "$ZI_HOME/$MOD_HOME/Src" )
+[0m [38;5;51m zmodload zi/zpmod
+
+[38;5;219m▓▒░[0m [38;5;220mSee 'zpmod -h' for more information.
+[38;5;219m▓▒░[0m [38;5;220mRun 'zpmod source-study' to see profile data,
+[38;5;219m▓▒░[0m [38;5;177mGuaranteed, automatic compilation of any sourced script.
+EOF
+        else
+          printf '%s\n' "${col_error}Module didn't build.$col_rst. You can copy the error messages and submit"
+          printf '%s\n' "error-report at: https://github.com/z-shell/zpmod/issues"
+        fi
+      fi
+    fi
+  else
+    printf '%s\n' "${col_error} Zsh is not installed. Please install zsh and try again.$col_rst"
+  fi
+}
+
+MAIN() {
+
+  col_pname="[33m"
+  col_error="[31m"
+  col_info="[32m"
+  col_info2="[32m"
+  col_rst="[0m"
+
+  ZI_HOME="${ZI_HOME:-${ZDOTDIR:-${HOME}}/.zi}"
+  MOD_HOME="${MOD_HOME:-zmodules}/zpmod"
+
+  printf '%s\n' "${col_info}Re-run this script to update (from Github) and rebuild the module.$col_rst"
+  printf '%s\n' "${col_info2}Press any key to continue, or Ctrl-C to exit.$col_rst"
+  read -r
+
+  setup_zpmod_repository
+  build_zpmod_module "$@"
+}
+
+while true; do
+  MAIN "$@"
+  exit 0
+done
