85364d8363475494be9bfa6e226b3a612f41794c
Salvydas Lukosius <sall@w-ss.io>
2023-01-02 21:16:02 +0000

    new file:   .github/.cspell/project-ignored.txt 	new file:   .github/.cspell/project-words.txt 	modified:   build.sh

--- build.sh
+++ build.sh
@@ -4,21 +4,39 @@
 # Clone or pull
 #
 
-setup_zpmod_repository() {
-  if ! test -d "${ZI_HOME}/${MOD_HOME}"; then
-    mkdir -p "${ZI_HOME}/${MOD_HOME}"
-    chmod g-rwX "${ZI_HOME}/${MOD_HOME}"
+setup_environment() {
+  if [ -z "$ZI_HOME" ]; then
+    if [ -d "$HOME"/.zi ]; then
+      ZI_HOME="${HOME}/.zi"
+    elif [ -d "$ZDOTDIR"/.zi ]; then
+      ZI_HOME="${ZDOTDIR}/.zi"
+    elif [ -d "$XDG_DATA_HOME"/.zi ]; then
+      ZI_HOME="${XDG_DATA_HOME}/.zi"
+    else
+      ZI_HOME="${HOME}/.zi"
+    fi
   fi
+  if [ -z "$MOD_HOME" ]; then
+    MOD_HOME="${ZI_HOME}/zmodules/zpmod"
+  fi
+  if ! test -d "${MOD_HOME}"; then
+    mkdir -p "${MOD_HOME}"
+    chmod g-rwX "${MOD_HOME}"
+  fi
+  if [ ! -d "$MOD_HOME" ]; then
+    printf '%s\n' "$col_error== Error: Failed to setup module directory ==$col_rst"
+    exit 1
+  fi
+}
 
-  printf '%s\n' "$col_pname== Downloading ZPMOD module to ${ZI_HOME}/${MOD_HOME}"
-  if test -d "${ZI_HOME}/${MOD_HOME}/.git"; then
-    cd "${ZI_HOME}/${MOD_HOME}" || return
+setup_zpmod_repository() {
+  printf '%s\n' "$col_pname== Downloading ZPMOD module to ${MOD_HOME}"
+  if test -d "${MOD_HOME}/.git"; then
+    builtin cd "${MOD_HOME}" || exit
     git pull -q origin main
   else
-    cd "$ZI_HOME" || return
     git clone -q https://github.com/z-shell/zpmod.git "$MOD_HOME"
   fi
-  printf '%s\n' "$col_pname== Done"
 }
 
 #
@@ -27,7 +45,7 @@ setup_zpmod_repository() {
 
 build_zpmod_module() {
   if command -v zsh >/dev/null; then
-    printf '%s\n' "$col_info2-- Checkig version --$col_rst"
+    printf '%s\n' "$col_info2-- Checking version --$col_rst"
     ZSH_CURRENT=$(zsh --version </dev/null | head -n1 | cut -d" " -f2,6- | tr -d '-')
     ZSH_REQUIRED="5.8"
     if expr "$ZSH_CURRENT" \< "$ZSH_REQUIRED" >/dev/null; then
@@ -36,9 +54,9 @@ build_zpmod_module() {
     else
       (
         printf '%s\n' "$col_info2-- Zsh version $ZSH_CURRENT --$col_rst"
-        builtin cd "${ZI_HOME}/${MOD_HOME}" || exit
+        builtin cd "${MOD_HOME}" || exit
         printf '%s\n' "$col_pname== Building module ZPMOD, running: a make clean, then ./configure and then make ==$col_rst"
-        printf '%s\n' "$col_pname== The module sources are located at: ${ZI_HOME}/${MOD_HOME} ==$col_rst"
+        printf '%s\n' "$col_pname== The module sources are located at: ${MOD_HOME} ==$col_rst"
         if [ -f Makefile ]; then
           if [ "$1" = "--clean" ]; then
             printf '%s\n' "$col_info2-- make distclean --$col_rst"
@@ -50,15 +68,15 @@ build_zpmod_module() {
           fi
         fi
         printf '%s\n' "$col_info2-- Configuring --$col_rst"
-        CPPFLAGS=-I/usr/local/include CFLAGS="-g -Wall -O3" LDFLAGS=-L/usr/local/lib ./configure --disable-gdbm --without-tcsetpgrp
+        CPPFLAGS="-I/usr/local/include" CFLAGS="-g -Wall -O3" LDFLAGS="-L/usr/local/lib" ./configure --disable-gdbm --enable-libc-musl --without-tcsetpgrp
         printf '%s\n' "$col_info2-- Running make --$col_rst"
-        if make -s; then
-          [ -f Src/zi/zpmod.so ] && cp -vf Src/zi/zpmod.so Src/zi/zpmod.bundle
+        if make -j 4 >/dev/null && [ -f Src/zi/zpmod.so ]; then
+          cp -vf Src/zi/zpmod.so Src/zi/zpmod.bundle
           command cat <<-EOF
 [38;5;219m▓▒░[0m [38;5;220mModule [38;5;177mhas been built correctly.
 [38;5;219m▓▒░[0m [38;5;220mTo [38;5;160mload the module, add following [38;5;220m2 lines to [38;5;172m.zshrc, at top:
 
-[0m [38;5;51m module_path+=( "$ZI_HOME/$MOD_HOME/Src" )
+[0m [38;5;51m module_path+=( "$MOD_HOME/Src" )
 [0m [38;5;51m zmodload zi/zpmod
 
 [38;5;219m▓▒░[0m [38;5;220mSee 'zpmod -h' for more information.
@@ -82,21 +100,8 @@ MAIN() {
   col_info2="[32m"
   col_rst="[0m"
 
-  if [ -z "$ZI_HOME" ]; then
-    if [ -d "$HOME"/.zi ]; then
-      ZI_HOME="${HOME}/.zi"
-    elif [ -d "$ZDOTDIR"/.zi ]; then
-      ZI_HOME="${ZDOTDIR}/.zi"
-    elif [ -d "$XDG_DATA_HOME"/.zi ]; then
-      ZI_HOME="${XDG_DATA_HOME}/.zi"
-    else
-      ZI_HOME="${HOME}/.zi"
-    fi
-  fi
-
-  MOD_HOME="${MOD_HOME:-zmodules}/zpmod"
-
-  setup_zpmod_repository
+  setup_environment "$@"
+  setup_zpmod_repository "$@"
   build_zpmod_module "$@"
   exit 0
 }
