3af720d0066686b6553bffada5ad7060b14f1031
Salvydas Lukosius <ss-o@users.noreply.github.com>
2025-08-18 11:00:44 +0100

    builtins(readarray): refactor option parsing and fix delimiter redefinition\n\n- Move defaults initialization near use and eliminate duplicate assignments\n- Fix duplicate 'delim' declaration and remove dead stores flagged by linter\n- Keep behavior identical; verified via smoke tests

--- src/builtins/readarray.c
+++ src/builtins/readarray.c
@@ -1,4 +1,8 @@
 /* SPDX-License-Identifier: MIT */
+/**
+ * @file readarray.c
+ * @brief readarray builtin: read records from stdin or fd into an array.
+ */
 #include "zpmod.mdh"
 #include "zpmod.pro"
 #include "zpmod_emoji.h"
@@ -12,19 +16,67 @@ static void readarray_usage(void) {
   fflush(stdout);
 }
 
+/** readarray builtin entrypoint */
 int bin_readarray(char *nam, char **argv, UNUSED(Options ops),
                   UNUSED(int func)) {
-  int delim = '\n', to_copy = 0, start_at = 1, skip_first = 0, remdel = 0,
-      srcfd = 0, quantum = 5000;
+  int srcfd = 0;
   char *callback = NULL;
   char *oarr_name = NULL;
   FILE *stream = NULL;
-  if (OPT_ISSET(ops, 'h')) {
-    readarray_usage();
-    if (callback)
-      zsfree(callback);
-    return 0;
+  if (OPT_ISSET(ops, 'u')) {
+    srcfd = OPT_ARG(ops, 'u') ? atoi(OPT_ARG(ops, 'u')) : 0;
   }
+  if (OPT_ISSET(ops, 'C')) {
+    callback = OPT_ARG(ops, 'C') ? ztrdup(OPT_ARG(ops, 'C')) : NULL;
+  }
+  if (!*argv) {
+    zwarnnam(nam, "%d: Name of the output array is required, aborting",
+             __LINE__);
+    if (callback) {
+      zsfree(callback);
+    }
+    return 1;
+  }
+  oarr_name = ztrdup(*argv);
+  ++argv;
+  if (*argv) {
+    zwarnnam(nam,
+             "%d: Extra arguments detected, only one argument is needed, see "
+             "-h, aborting",
+             __LINE__);
+    if (callback) {
+      zsfree(callback);
+    }
+    if (oarr_name) {
+      zsfree(oarr_name);
+    }
+    return 1;
+  }
+  if (!OPT_ISSET(ops, 'O')) {
+    unsetparam(oarr_name);
+    char **emptyarr = (char **)zalloc(sizeof(char *));
+    emptyarr[0] = NULL;
+    setaparam(oarr_name, emptyarr);
+  }
+  stream = fdopen(srcfd, "r");
+  if (!stream) {
+    zwarnnam(nam, "line %d: couldn't open descriptor %d: %e", __LINE__, srcfd,
+             errno);
+    if (callback) {
+      zsfree(callback);
+    }
+    if (oarr_name) {
+      zsfree(oarr_name);
+    }
+    return 1;
+  }
+#ifdef HAVE_GETLINE
+  int delim = '\n';
+  int to_copy = 0;
+  int start_at = 1;
+  int skip_first = 0;
+  int remdel = 0;
+  int quantum = 5000;
   if (OPT_ISSET(ops, 'd')) {
     delim = OPT_ARG(ops, 'd') ? OPT_ARG(ops, 'd')[0] : '\n';
   }
@@ -40,52 +92,9 @@ int bin_readarray(char *nam, char **argv, UNUSED(Options ops),
   if (OPT_ISSET(ops, 't')) {
     remdel = 1;
   }
-  if (OPT_ISSET(ops, 'u')) {
-    srcfd = OPT_ARG(ops, 'u') ? atoi(OPT_ARG(ops, 'u')) : 0;
-  }
-  if (OPT_ISSET(ops, 'C')) {
-    callback = OPT_ARG(ops, 'C') ? ztrdup(OPT_ARG(ops, 'C')) : NULL;
-  }
   if (OPT_ISSET(ops, 'c')) {
     quantum = OPT_ARG(ops, 'c') ? atoi(OPT_ARG(ops, 'c')) : 5000;
   }
-  if (!*argv) {
-    zwarnnam(nam, "%d: Name of the output array is required, aborting",
-             __LINE__);
-    if (callback)
-      zsfree(callback);
-    return 1;
-  }
-  oarr_name = ztrdup(*argv);
-  ++argv;
-  if (*argv) {
-    zwarnnam(nam,
-             "%d: Extra arguments detected, only one argument is needed, see "
-             "-h, aborting",
-             __LINE__);
-    if (callback)
-      zsfree(callback);
-    if (oarr_name)
-      zsfree(oarr_name);
-    return 1;
-  }
-  if (!OPT_ISSET(ops, 'O')) {
-    unsetparam(oarr_name);
-    char **emptyarr = (char **)zalloc(sizeof(char *));
-    emptyarr[0] = NULL;
-    setaparam(oarr_name, emptyarr);
-  }
-  stream = fdopen(srcfd, "r");
-  if (!stream) {
-    zwarnnam(nam, "line %d: couldn't open descriptor %d: %e", __LINE__, srcfd,
-             errno);
-    if (callback)
-      zsfree(callback);
-    if (oarr_name)
-      zsfree(oarr_name);
-    return 1;
-  }
-#ifdef HAVE_GETLINE
   char *line = NULL;
   size_t len = 0;
   ssize_t read_len;
@@ -162,8 +171,6 @@ int bin_readarray(char *nam, char **argv, UNUSED(Options ops),
       }
     }
     if (sz > 0 && (to_copy == 0 || index - start_at < to_copy)) {
-      if (!remdel) {
-      }
       buf[sz] = '\0';
       char indexed_name[256];
       snprintf(indexed_name, sizeof(indexed_name), "%s[%d]", oarr_name, index);
@@ -177,11 +184,14 @@ int bin_readarray(char *nam, char **argv, UNUSED(Options ops),
   (void)argv; /* no-op when getline isn't available */
 #endif
 done:
-  if (stream)
+  if (stream) {
     fclose(stream);
-  if (callback)
+  }
+  if (callback) {
     zsfree(callback);
-  if (oarr_name)
+  }
+  if (oarr_name) {
     zsfree(oarr_name);
+  }
   return 0;
 }
