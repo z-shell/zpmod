6a11e332031faaf7d4650b1c87fb35cacf47fc20
Salvydas Lukosius <ss-o@users.noreply.github.com>
2025-08-18 13:46:46 +0100

    module(zpmod): conditionally include source.c and define ZPMOD_HAVE_SOURCE_STUDY
    Signed-off-by: Salvydas Lukosius <ss-o@users.noreply.github.com>

--- src/module/zpmod_imports.h
+++ src/module/zpmod_imports.h
@@ -9,63 +9,6 @@
 #ifndef ZPMOD_ZSH_IMPORTS_H
 #define ZPMOD_ZSH_IMPORTS_H
 
-/* Parameter functions from params.c */
-#ifndef mod_import_function
-Param setaparam(char *s, char **aval);
-char *getsparam(char *s);
-char **getaparam(char *s);
-Param setsparam(char *s, char *val);
-void unsetparam(char *s);
-#else
-mod_import_function Param setaparam(char *s, char **aval);
-mod_import_function char *getsparam(char *s);
-mod_import_function char **getaparam(char *s);
-mod_import_function Param setsparam(char *s, char *val);
-mod_import_function void unsetparam(char *s);
-#endif
-
-/* Memory management functions from mem.c */
-#ifndef mod_import_function
-void *zalloc(size_t size);
-void zfree(void *p, int sz);
-void zsfree(char *p);
-void *zrealloc(void *ptr, size_t size);
-#else
-mod_import_function void *zalloc(size_t size);
-mod_import_function void zfree(void *p, int sz);
-mod_import_function void zsfree(char *p);
-mod_import_function void *zrealloc(void *ptr, size_t size);
-#endif
-
-/* String/metafication functions from utils.c */
-#ifndef mod_import_function
-char *metafy(char *buf, int len, int heap);
-char *unmetafy(char *s, int *len);
-void zwarnnam(const char *cmd, const char *fmt, ...);
-#else
-mod_import_function char *metafy(char *buf, int len, int heap);
-mod_import_function char *unmetafy(char *s, int *len);
-mod_import_function void zwarnnam(const char *cmd, const char *fmt, ...);
-#endif
-
-/* Option functions from options.c */
-#ifndef mod_import_function
-int optlookup(const char *name);
-#else
-mod_import_function int optlookup(const char *name);
-#endif
-
-/* Module feature functions from module.c */
-#ifndef mod_import_function
-char **featuresarray(Module m, Features f);
-int handlefeatures(Module m, Features f, int **enables);
-int setfeatureenables(Module m, Features f, int *e);
-#else
-mod_import_function char **featuresarray(Module m, Features f);
-mod_import_function int handlefeatures(Module m, Features f, int **enables);
-mod_import_function int setfeatureenables(Module m, Features f, int *e);
-#endif
-
 #if defined(__has_include)
 #if __has_include("builtin.epro")
 #include "builtin.epro"
